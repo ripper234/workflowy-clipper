c41b1adead64c9eeadc1a0c2b35ea431350a0b43
Ron Gross <chiefninjaofficer@gmail.com>
2016-06-25 18:45:07 +0300

    Rename #message to #rawhtml

--- src/popup.js
+++ src/popup.js
@@ -155,20 +155,20 @@ function showErrMsg(msg) {
 
 chrome.runtime.onMessage.addListener(function(request, sender) {
   if (request.action == "getSource") {
-    message.innerText = request.source;
+    rawHtml.innerText = request.source;
   }
 });
 
 function onWindowLoad() {
 
-  var message = document.querySelector('#message');
+  var rawHtml = document.querySelector('#rawHtml');
 
   chrome.tabs.executeScript(null, {
     file: "getPageSource.js"
   }, function() {
     // If you try and inject into an extensions page or the webstore/NTP you'll get an error
     if (chrome.runtime.lastError) {
-      message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
+      rawHtml.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
     }
   });
 
